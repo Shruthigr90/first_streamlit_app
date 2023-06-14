cba1a449f554dee055de3565c93a907462612e18
Shruthigr90 <136318579+Shruthigr90@users.noreply.github.com>
2023-06-14 08:36:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,5 @@ Streamlit.multiselect("pick some fruits :",list(my_fruit_list.index),['Avocado',
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+streamlit.header("Fruityvice Fruit Advice!")
