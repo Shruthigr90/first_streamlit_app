120ae3b9259992fda683aa531aea4290963f027d
Shruthigr90 <136318579+Shruthigr90@users.noreply.github.com>
2023-06-14 08:51:07 +0530

    Update streamlit_app.py
    added breakfast menu header & 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 streamlit.title('my parents New healthy diner')
-streamlit.header('BREAKFAST MENU')
-streamlit.text('Omega 3 & blueberry oatmeal')
-streamlit.text('Kale , Spinach Rocket Smoothie')
-streamlit.text('Hard-Boiled Free range Egg')
+streamlit.header('BREAKFAST Favorite')
+streamlit.text('🥣Omega 3 & blueberry oatmeal')
+streamlit.text('🥗Kale , Spinach Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -19,3 +20,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
